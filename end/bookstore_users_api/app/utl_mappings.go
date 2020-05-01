cbd10a8b15f91b7dc0fa1eb94acaea07f46b81f9
teng <wwwsousoucom@126.com>
2020-05-01 20:51:46 +0800

    user

--- end/bookstore_users_api/app/utl_mappings.go
+++ end/bookstore_users_api/app/utl_mappings.go
@@ -1,7 +1,12 @@
 package app
 
-import "ea/end/bookstore_users_api/controllers"
+import (
+	"ea/end/bookstore_users_api/controllers/ping"
+	"ea/end/bookstore_users_api/controllers/user"
+)
 
 func mapUrls() {
-	router.GET("/ping", controllers.Ping)
+	router.GET("/ping", ping.Ping)
+	router.GET("/users/:user_id", user.GetUser)
+	router.POST("/users", user.CreateUser)
 }
