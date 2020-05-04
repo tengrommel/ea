9687eff670759af6abe1e0ad10df5ee3e0728f9b
teng <wwwsousoucom@126.com>
2020-05-04 17:07:19 +0800

    最高

---
+++ leetcode/3_longest_substring_without_repeating_characters/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+Given a string, find the length of the longest substringwithout repeating characters.
+
+*/
+
+func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	var freq [256]int
+	result := 0
+	left, right := 0, -1
+
+	for left < len(s) {
+		if right+1 < len(s) && freq[s[right+1]-'a'] == 0 {
+			freq[s[right+1]-'a']++
+			right++
+		} else {
+			freq[s[left]-'a']--
+			left++
+		}
+		result = max(result, right-left+1)
+	}
+	return result
+}
+
+func max(result int, i int) int {
+	if result > i {
+		return result
+	}
+	return i
+}
+
+func main() {
+	fmt.Println(lengthOfLongestSubstring("dddfa"))
+}
