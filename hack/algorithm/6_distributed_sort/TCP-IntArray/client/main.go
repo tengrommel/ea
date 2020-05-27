e128a7f48600d6177a20476f313e9d881a3a2a20
teng <wwwsousoucom@126.com>
2020-05-27 20:09:25 +0800

    codec

---
+++ hack/algorithm/6_distributed_sort/TCP-IntArray/client/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+	"time"
+)
+
+func IntToBytes(n int) []byte {
+	data := int64(n)                                 // 数据类型的转换
+	byteBuffer := bytes.NewBuffer([]byte{})          // 字节集合
+	binary.Write(byteBuffer, binary.BigEndian, data) // 按照二进制写入字节
+	return byteBuffer.Bytes()
+}
+
+func BytesToInt(bs []byte) int {
+	byteBuffer := bytes.NewBuffer(bs) // 根据二进制写入二进制结合
+	var data int64
+	binary.Read(byteBuffer, binary.BigEndian, &data)
+	return int(data)
+}
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:8848")
+	defer conn.Close()
+	if err != nil {
+		fmt.Println("客户端连接失败")
+		return
+	}
+	time.Sleep(1 * time.Second)
+	// 等待给服务器输入信息
+	go func() {
+		// 开始
+		myBytesStart := IntToBytes(0)
+		myBytesStart = append(myBytesStart, IntToBytes(0)...)
+		conn.Write(myBytesStart)
+
+		myArray := []int{1, 9, 2, 8, 3, 7, 6, 4, 5, 0}
+		for i := 0; i < len(myArray); i++ {
+			myBytesData := IntToBytes(1)
+			myBytesData = append(myBytesData, IntToBytes(myArray[i])...)
+			conn.Write(myBytesData)
+		}
+		// 结束
+		myBytesEnd := IntToBytes(0)
+		myBytesEnd = append(myBytesEnd, IntToBytes(1)...)
+		conn.Write(myBytesEnd)
+	}()
+
+	arr := make([]int, 0)
+	for {
+		// 等待，接收信息
+		buf := make([]byte, 16)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("服务器关闭")
+			return
+		}
+		if n == 16 {
+			data1 := BytesToInt(buf[:len(buf)/2]) // 取出第一个数
+			data2 := BytesToInt(buf[len(buf)/2:]) // 取出第二个数
+			if data1 == 0 && data2 == 0 {
+				// 开始
+				arr = make([]int, 0, 0)
+			}
+			if data1 == 1 {
+				// 接收数组
+				arr = append(arr, data2)
+			}
+			if data1 == 0 && data2 == 1 {
+				// 结束
+				fmt.Println("收到数组 arr:", arr)
+				arr = nil
+			}
+		}
+	}
+}
---
+++ hack/algorithm/6_distributed_sort/TCP-IntArray/server/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+	"sort"
+)
+
+func IntToBytes(n int) []byte {
+	data := int64(n)                                 // 数据类型的转换
+	byteBuffer := bytes.NewBuffer([]byte{})          // 字节集合
+	binary.Write(byteBuffer, binary.BigEndian, data) // 按照二进制写入字节
+	return byteBuffer.Bytes()
+}
+
+func BytesToInt(bs []byte) int {
+	byteBuffer := bytes.NewBuffer(bs) // 根据二进制写入二进制结合
+	var data int64
+	binary.Read(byteBuffer, binary.BigEndian, &data)
+	return int(data)
+}
+
+func Server(conn net.Conn) {
+	if conn == nil {
+		fmt.Println("无效连接")
+		return
+	}
+	// 接收数据，处理
+	arr := make([]int, 0)
+	for {
+		// 等待，接收信息
+		buf := make([]byte, 16)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("服务器关闭")
+			return
+		}
+		if n == 16 {
+			data1 := BytesToInt(buf[:len(buf)/2]) // 取出第一个数
+			data2 := BytesToInt(buf[len(buf)/2:]) // 取出第二个数
+			if data1 == 0 && data2 == 0 {
+				// 开始
+				arr = make([]int, 0, 0)
+			}
+			if data1 == 1 {
+				// 接收数组
+				arr = append(arr, data2)
+			}
+			if data1 == 0 && data2 == 1 {
+				// 结束
+				fmt.Println("收到数组 arr:", arr)
+				sort.Ints(arr)
+				fmt.Println("数组排序完成", arr)
+				// 返回结果
+				myBytesStart := IntToBytes(0)
+				myBytesStart = append(myBytesStart, IntToBytes(0)...)
+				conn.Write(myBytesStart)
+
+				myArray := arr
+				for i := 0; i < len(myArray); i++ {
+					myBytesData := IntToBytes(1)
+					myBytesData = append(myBytesData, IntToBytes(myArray[i])...)
+					conn.Write(myBytesData)
+				}
+				// 结束
+				myBytesEnd := IntToBytes(0)
+				myBytesEnd = append(myBytesEnd, IntToBytes(1)...)
+				conn.Write(myBytesEnd)
+			}
+		}
+	}
+}
+
+func main() {
+	server, err := net.Listen("tcp", "127.0.0.1:8848")
+	defer server.Close()
+	if err != nil {
+		fmt.Println("服务器开启失败")
+		return
+	}
+	fmt.Println("正在开启服务器...")
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			fmt.Println("链接出错")
+		}
+		go Server(conn) // 并发处理
+	}
+}
