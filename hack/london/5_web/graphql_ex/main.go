dba52e7704c4182c8b562744c53271cb7f555d1d
teng <wwwsousoucom@126.com>
2020-06-10 18:09:53 +0800

    finish

--- hack/london/5_web/graphql_ex/main.go
+++ hack/london/5_web/graphql_ex/main.go
@@ -1,7 +1,103 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/graphql-go/graphql"
+	"net/http"
+)
+
+var authors []Author = []Author{
+	{
+		Id:        "author-1",
+		FirstName: "Nic",
+		LastName:  "Raboy",
+		UserName:  "nraboy",
+		Password:  "pass",
+	},
+	{
+		Id:        "author-2",
+		FirstName: "Maria",
+		LastName:  "Raboy",
+		UserName:  "Mraboy",
+		Password:  "abc",
+	},
+}
+
+var articles []Article = []Article{
+	{
+		Id:      "author-1",
+		Author:  "author-1",
+		Title:   "Blog Post 1",
+		Content: " this is an example",
+	},
+}
+
+var rootQuery *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Query",
+	Fields: graphql.Fields{
+		"authors": &graphql.Field{
+			Type: graphql.NewList(authorType),
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				return authors, nil
+			},
+		},
+		"author": &graphql.Field{
+			Type: authorType,
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				id := params.Args["id"].(string)
+				for _, author := range authors {
+					if author.Id == id {
+						return author, nil
+					}
+				}
+				return nil, nil
+			},
+		},
+		"articles": &graphql.Field{
+			Type: graphql.NewList(articleType),
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				return articles, nil
+			},
+		},
+		"article": &graphql.Field{
+			Type: articleType,
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				id := params.Args["id"].(string)
+				for _, article := range articles {
+					if article.Id == id {
+						return article, nil
+					}
+				}
+				return nil, nil
+			},
+		},
+	},
+})
 
 func main() {
 	fmt.Println("starting graphql application...")
+	router := mux.NewRouter()
+	schema, _ := graphql.NewSchema(
+		graphql.SchemaConfig{
+			Query: rootQuery,
+		})
+	router.HandleFunc("/graphql", func(response http.ResponseWriter, request *http.Request) {
+		response.Header().Set("content-type", "application/json")
+		result := graphql.Do(graphql.Params{
+			Schema: schema,
+		})
+		json.NewEncoder(response).Encode(result)
+	})
 }
