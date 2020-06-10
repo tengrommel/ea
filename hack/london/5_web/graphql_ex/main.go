7b8cb88ebcf61fe82972dde0ad17b6e7bb4f8639
teng <wwwsousoucom@126.com>
2020-06-10 20:54:20 +0800

    finish

--- hack/london/5_web/graphql_ex/main.go
+++ hack/london/5_web/graphql_ex/main.go
@@ -5,8 +5,15 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/graphql-go/graphql"
+	"github.com/mitchellh/mapstructure"
+	"github.com/satori/go.uuid"
 	"net/http"
 )
+
+type GraphQLPayload struct {
+	Query     string                 `json:"query"`
+	Variables map[string]interface{} `json:"variables"`
+}
 
 var authors []Author = []Author{
 	{
@@ -85,6 +92,79 @@ var rootQuery *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
 		},
 	},
 })
+var rootMotation *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Mutation",
+	Fields: graphql.Fields{
+		"deleteAuthor": &graphql.Field{
+			Type: graphql.NewList(authorType),
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				id := params.Args["id"].(string)
+				for index, author := range authors {
+					if author.Id == id {
+						authors = append(authors[:index], authors[index+1:]...)
+						return authors, nil
+					}
+				}
+				return nil, nil
+			},
+		},
+		"updateAuthor": &graphql.Field{
+			Type: graphql.NewList(authorType),
+			Args: graphql.FieldConfigArgument{
+				"author": &graphql.ArgumentConfig{
+					Type: authorInputType,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				var changes Author
+				mapstructure.Decode(params.Args["author"], &changes)
+				for index, author := range authors {
+					if author.Id == changes.Id {
+						if changes.FirstName != "" {
+							author.FirstName = changes.FirstName
+						}
+						if changes.LastName != "" {
+							author.LastName = changes.LastName
+						}
+						if changes.FirstName != "" {
+							author.FirstName = changes.FirstName
+						}
+						if changes.UserName != "" {
+							author.UserName = changes.UserName
+						}
+						if changes.Password != "" {
+							author.Password = changes.Password
+						}
+						authors[index] = author
+						return authors, nil
+					}
+				}
+				return nil, nil
+			},
+		},
+		"createArticle": &graphql.Field{
+			Type: graphql.NewList(articleType),
+			Args: graphql.FieldConfigArgument{
+				"article": &graphql.ArgumentConfig{
+					Type: articleInputType,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				var article Article
+				mapstructure.Decode(params.Args["article"], &article)
+				article.Id = uuid.Must(uuid.NewV4(), nil).String()
+				article.Author = "teng"
+				articles = append(articles, article)
+				return articles, nil
+			},
+		},
+	},
+})
 
 func main() {
 	fmt.Println("starting graphql application...")
@@ -95,9 +175,13 @@ func main() {
 		})
 	router.HandleFunc("/graphql", func(response http.ResponseWriter, request *http.Request) {
 		response.Header().Set("content-type", "application/json")
+		var payload GraphQLPayload
 		result := graphql.Do(graphql.Params{
-			Schema: schema,
+			Schema:         schema,
+			RequestString:  payload.Query,
+			VariableValues: payload.Variables,
 		})
 		json.NewEncoder(response).Encode(result)
 	})
+	http.ListenAndServe(":12345", router)
 }
