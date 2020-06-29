55feaa44ba9dfd0bc5def53cd093b778d6ad0773
teng <wwwsousoucom@126.com>
2020-06-29 15:57:17 +0800

    对称

--- encryption/crypto_cbc/main.go
+++ encryption/crypto_cbc/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/cipher"
 	"crypto/des"
+	"fmt"
 )
 
 func paddingLastGroup(plainText []byte, blockSize int) []byte {
@@ -14,15 +15,16 @@ func paddingLastGroup(plainText []byte, blockSize int) []byte {
 	// 切片创建，并初始化
 	newPlain := bytes.Repeat(char, padNum)
 	// 3、newPlain数组追加到原始明文的后面
-	return append(plainText, newPlain...)
+	newText := append(plainText, newPlain...)
+	return newText
 }
 
 func unPaddingLastGroup(plainText []byte) []byte {
 	// 1、拿去切片中的最后一个字节
-	lenght := len(plainText)
-	lastChar := plainText[lenght-1] // byte类型
+	length := len(plainText)
+	lastChar := plainText[length-1] // byte类型
 	number := int(lastChar)         // 尾部填充的字节数
-	return plainText[:lenght-number]
+	return plainText[:length-number]
 }
 
 // des 加密
@@ -36,7 +38,6 @@ func desEncrypt(plainText, key []byte) []byte {
 	newText := paddingLastGroup(plainText, block.BlockSize())
 	// 3、创建一个使用cbc分组接口
 	iv := []byte("12345678")
-	cipher.NewCBCEncrypter(block, iv)
 	blockMode := cipher.NewCBCEncrypter(block, iv)
 	// 4、加密
 	cipherText := make([]byte, len(newText))
@@ -44,7 +45,27 @@ func desEncrypt(plainText, key []byte) []byte {
 	return cipherText
 }
 
-// des的CBC加密
-func main() {
+// des的解密
+func desDecrypt(cipherText, key []byte) []byte {
+	block, err := des.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+	// 2、创建一个使用cbc解密的接口
+	iv := []byte("12345678")
+	blockMode := cipher.NewCBCDecrypter(block, iv)
+	// 3、解密
+	blockMode.CryptBlocks(cipherText, cipherText)
+	// 4、cipherText 存储的是明文
+	plainText := unPaddingLastGroup(cipherText)
+	return plainText
+}
 
+func main() {
+	fmt.Println("Des 加密 解密")
+	key := []byte("01234567")
+	src := []byte("戀愛靠機會，分手靠智慧。曹操歷經滄桑，心裡想的大概是：好頭不如好尾。")
+	cipherText := desEncrypt(src, key)
+	plainText := desDecrypt(cipherText, key)
+	fmt.Printf("解密之后的数据：%s\n", string(plainText))
 }
