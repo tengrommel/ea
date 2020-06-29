d8bc6dc3b94b0685484f92e38df1d460cb2a3e4b
teng <wwwsousoucom@126.com>
2020-06-29 11:44:41 +0800

    des cbc加密实现

--- encryption/crypto_cbc/main.go
+++ encryption/crypto_cbc/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"crypto/cipher"
+	"crypto/des"
+)
 
 func paddingLastGroup(plainText []byte, blockSize int) []byte {
 	// 1、求出最后一个组中剩余的字节数 28 % 8 3 .. 4
@@ -11,6 +15,33 @@ func paddingLastGroup(plainText []byte, blockSize int) []byte {
 	newPlain := bytes.Repeat(char, padNum)
 	// 3、newPlain数组追加到原始明文的后面
 	return append(plainText, newPlain...)
+}
+
+func unPaddingLastGroup(plainText []byte) []byte {
+	// 1、拿去切片中的最后一个字节
+	lenght := len(plainText)
+	lastChar := plainText[lenght-1] // byte类型
+	number := int(lastChar)         // 尾部填充的字节数
+	return plainText[:lenght-number]
+}
+
+// des 加密
+func desEncrypt(plainText, key []byte) []byte {
+	// 1、建一个底层使用des的密码接口
+	block, err := des.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+	// 2、明文填充
+	newText := paddingLastGroup(plainText, block.BlockSize())
+	// 3、创建一个使用cbc分组接口
+	iv := []byte("12345678")
+	cipher.NewCBCEncrypter(block, iv)
+	blockMode := cipher.NewCBCEncrypter(block, iv)
+	// 4、加密
+	cipherText := make([]byte, len(newText))
+	blockMode.CryptBlocks(cipherText, newText)
+	return cipherText
 }
 
 // des的CBC加密
